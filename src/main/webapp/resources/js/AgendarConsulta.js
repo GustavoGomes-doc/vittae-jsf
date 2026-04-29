54d7c68afc0bf212d7814f487f41b866a589d0f6
Seu Nome Aqui <seu.email@exemplo.com>
2026-04-29 19:22:12 -0300

    m

--- src/main/webapp/resources/js/AgendarConsulta.js
+++ src/main/webapp/resources/js/AgendarConsulta.js
@@ -1,3 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+const hoje = new Date();
+            const opcoes = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+            document.getElementById('dataHoje').textContent =
+                hoje.toLocaleDateString('pt-BR', opcoes);
 
+document.addEventListener('DOMContentLoaded', () => {
+	const hoje = new Date();
+	            const opcoes = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	            document.getElementById('dataHoje').textContent =
+	                hoje.toLocaleDateString('pt-BR', opcoes);
 });
